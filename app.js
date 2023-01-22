d2a585c29cffe101e9a567cf29fdc7dab2a97d05
Jean Flores <jeancarlo_flores@hotmail.com>
2023-01-21 20:13:23 -0500

    Install prom

--- app.js
+++ app.js
@@ -12,10 +12,31 @@ const app = express()
 // })
 
 const appPromise = require('./src/app/middlewares/configprovider').appPromise
+
+const client = require('prom-client');
+const counter = new client.Counter({
+    name: 'http_request_counter_health',
+    help: 'Metrics to count hits on the health api ',
+});
+const register = new client.Registry();
+register.registerMetric(counter)
+register.setDefaultLabels({
+    app: 'app-withdrawal',
+    prefix: 'node_'
+  })
+client.collectDefaultMetrics({ register })
  
 appPromise.then(function(app) {
     const PORT = process.env.SERVER_PORT_WITHDRAWAL || 3004
     app.use('/api', require('./src/app/routes'))
+    app.get('/metrics', async (req, res) => {
+        res.setHeader('Content-Type', register.contentType)
+        res.send(await register.metrics())
+    });
+    app.get('/health', async (req, res) => {
+        counter.inc()
+        res.send({message: 'Account App healty'});
+    });
     app.listen(PORT, () => {
         console.log('Application running on port ', PORT)
     })
